20113904f4c56f936faa8951aa8902bdca72d0c3
romo <robert.moore@zuhlke.com>
2019-10-30 12:41:29 +0000

    rename

--- src/test/kotlin/com/zuhlke/rob/PianoNoteTest.kt
+++ src/test/kotlin/com/zuhlke/rob/PianoNoteTest.kt
@@ -11,8 +11,8 @@ import java.time.temporal.ChronoUnit.SECONDS
 class PianoNoteTest {
     @Test
     fun `play duration equals number of beats divided by beats per minute divided by seconds per minute`() {
-        val mockSample = mockk<Sample>("piano sample")
-        val pianoNote = PianoNote(mockSample, 1, 60)
+        val dummySample = mockk<Sample>("piano sample")
+        val pianoNote = PianoNote(dummySample, 1, 60)
 
         assertThat(pianoNote.duration(), equalTo(Duration.of(1, SECONDS)))
     }
