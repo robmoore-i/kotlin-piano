10d12aa789c59053fcec33dc60c06fc8d456ab86
romo <robert.moore@zuhlke.com>
2019-11-01 13:40:19 +0000

    not td

--- src/main/kotlin/com/zuhlke/rob/Player.kt
+++ src/main/kotlin/com/zuhlke/rob/Player.kt
@@ -30,28 +30,3 @@ class FullPlayer(private val lock: Lock) : Player {
         lock.release()
     }
 }
-
-class CutoffPlayer(private val lock: Lock) : Player {
-    private lateinit var clip: Clip
-
-    override fun update(event: LineEvent?) {
-        if (event == null) {
-            return
-        }
-        if (event.type == LineEvent.Type.STOP) {
-            onStop()
-        }
-    }
-
-    override fun play(clip: Clip) {
-        this.clip = clip
-        clip.startWithPlaybackListener(this)
-
-        lock.block { clip.isComplete() }
-    }
-
-    private fun onStop() {
-        clip.stop()
-        lock.release()
-    }
-}
