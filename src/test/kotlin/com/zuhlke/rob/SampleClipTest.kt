55445459f6f5d99531b63ce189f1d576db3a4805
romo <robert.moore@zuhlke.com>
2019-11-01 12:19:29 +0000

    more

--- src/test/kotlin/com/zuhlke/rob/SampleClipTest.kt
+++ src/test/kotlin/com/zuhlke/rob/SampleClipTest.kt
@@ -1,16 +1,25 @@
 package com.zuhlke.rob
 
+import io.mockk.clearMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.Before
 import org.junit.Test
 import javax.sound.sampled.LineEvent
 
 class SampleClipTest {
+    private val mockPlaybackLock = mockk<PlaybackLock>(relaxed = true)
+    private val mockSample = mockk<Sample>(relaxed = true)
+
+    @Before
+    fun setUp() {
+        clearMocks(mockPlaybackLock, mockSample)
+    }
+
     @Test
     fun `when the clip starts it blocks on the playback lock`() {
-        val mockPlaybackLock = mockk<PlaybackLock>(relaxed = true)
-        val clip = SampleClip(mockPlaybackLock)
+        val clip = SampleClip(mockPlaybackLock, mockSample)
 
         clip.play()
 
@@ -19,8 +28,7 @@ class SampleClipTest {
 
     @Test
     fun `when updated with stop event it releases the playback lock`() {
-        val mockPlaybackLock = mockk<PlaybackLock>(relaxed = true)
-        val clip = SampleClip(mockPlaybackLock)
+        val clip = SampleClip(mockPlaybackLock, mockSample)
 
         clip.update(lineEvent(LineEvent.Type.STOP))
 
@@ -29,8 +37,7 @@ class SampleClipTest {
 
     @Test
     fun `when updated with a start event it does nothing`() {
-        val mockPlaybackLock = mockk<PlaybackLock>(relaxed = true)
-        val clip = SampleClip(mockPlaybackLock)
+        val clip = SampleClip(mockPlaybackLock, mockSample)
 
         clip.update(lineEvent(LineEvent.Type.START))
 
@@ -39,12 +46,29 @@ class SampleClipTest {
 
     @Test
     fun `when updated with a null event it does nothing`() {
-        val mockPlaybackLock = mockk<PlaybackLock>(relaxed = true)
-        val clip = SampleClip(mockPlaybackLock)
+        val clip = SampleClip(mockPlaybackLock, mockSample)
 
         clip.update(null)
 
         verify(exactly = 0) { mockPlaybackLock.release() }
+    }
+
+    @Test
+    fun `stops sample when stopped`() {
+        val clip = SampleClip(mockPlaybackLock, mockSample)
+
+        clip.update(lineEvent(LineEvent.Type.STOP))
+
+        verify { mockSample.stop() }
+    }
+
+    @Test
+    fun `starts sample when played using itself as the playback listener`() {
+        val clip = SampleClip(mockPlaybackLock, mockSample)
+
+        clip.play()
+
+        verify { mockSample.startWithPlaybackListener(clip) }
     }
 
     private fun lineEvent(type: LineEvent.Type): LineEvent {
@@ -54,12 +78,17 @@ class SampleClipTest {
     }
 }
 
+interface Sample {
+    fun startWithPlaybackListener(clip: AudioClip)
+    fun stop()
+}
+
 interface PlaybackLock {
     fun block()
     fun release()
 }
 
-class SampleClip(private val playbackLock: PlaybackLock) : AudioClip {
+class SampleClip(private val playbackLock: PlaybackLock, private val sample: Sample) : AudioClip {
     override fun update(event: LineEvent?) {
         if (event == null) {
             return
@@ -70,10 +99,12 @@ class SampleClip(private val playbackLock: PlaybackLock) : AudioClip {
     }
 
     override fun play() {
+        sample.startWithPlaybackListener(this)
         playbackLock.block()
     }
 
     private fun onStop() {
+        sample.stop()
         playbackLock.release()
     }
 }
