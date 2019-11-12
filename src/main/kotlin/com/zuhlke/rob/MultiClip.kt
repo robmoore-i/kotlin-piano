3df7483f596382b5f833c0c43dd0a78ce2368484
romo <robert.moore@zuhlke.com>
2019-11-12 11:28:34 +0000

    Put the tightly coupled corresponding Clip and Player classes in the same Kotlin file

--- src/main/kotlin/com/zuhlke/rob/MultiClip.kt
+++ src/main/kotlin/com/zuhlke/rob/MultiClip.kt
@@ -15,3 +15,19 @@ class MultiClip(private vararg val subclips: UniClip) : Clip<() -> UniClipPlayer
         return subclips.all { it.isComplete() }
     }
 }
+
+class MultiClipPlayer(private val uniClipPlayerProvider: () -> UniClipPlayer) : ClipPlayer<MultiClip, Semaphore> {
+    override fun play(clip: MultiClip, mutex: Semaphore) {
+        mutex.increment(clip.cardinality())
+        clip.playUsing {
+            val singleClipPlayer = uniClipPlayerProvider.invoke()
+            singleClipPlayer.addStopAction { mutex.decrement(1) }
+            singleClipPlayer
+        }
+        mutex.block()
+    }
+
+    override fun playInBackground(clip: MultiClip) {
+        clip.playUsing(uniClipPlayerProvider)
+    }
+}
--- src/main/kotlin/com/zuhlke/rob/UniClip.kt
+++ src/main/kotlin/com/zuhlke/rob/UniClip.kt
@@ -1,6 +1,8 @@
 package com.zuhlke.rob
 
 import javax.sound.sampled.AudioInputStream
+import javax.sound.sampled.LineEvent
+import javax.sound.sampled.LineListener
 
 typealias RawClip = javax.sound.sampled.Clip
 
@@ -26,5 +28,38 @@ class UniClip(private val audioInputStream: AudioInputStream, private val clip: 
 
     override fun isComplete(): Boolean {
         return complete
+    }
+}
+
+class UniClipPlayer : LineListener, ClipPlayer<UniClip, Lock> {
+    private val callbacks: MutableList<() -> Unit> = mutableListOf()
+    private lateinit var clip: UniClip
+    private lateinit var lock: Lock
+
+    override fun play(clip: UniClip, mutex: Lock) {
+        this.lock = mutex
+        playInBackground(clip)
+        mutex.block { clip.isComplete() }
+    }
+
+    override fun playInBackground(clip: UniClip) {
+        this.clip = clip
+        clip.playUsing(this)
+    }
+
+    override fun update(event: LineEvent) {
+        if (event.type == LineEvent.Type.STOP) {
+            onStop()
+        }
+    }
+
+    fun addStopAction(callback: () -> Unit) {
+        callbacks.add(callback)
+    }
+
+    private fun onStop() {
+        callbacks.forEach { it.invoke() }
+        clip.stop()
+        lock.release()
     }
 }
